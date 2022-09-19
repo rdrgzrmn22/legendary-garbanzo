9657b9a7b7eecaad5698ac2277f6d382640f2852
unknown <202080505@psu.palawan.edu.ph>
2022-09-19 23:27:15 +0800

    web 3 and 4 done

--- web4/index.php
+++ web4/index.php
@@ -1,7 +1,23 @@
 <?php
 // set cookies
+       if(isset($_POST['username'] )){
+        if(isset($_POST['password'])){
+        $username = htmlentities($_POST['username']);
+        $password = htmlentities($_POST['password']);
+        $user = empty($username);
+        $pass = empty($password);        
+        header('Location: loggedin.php');
+       
 
+        if($user != 1 & $pass != 1){
+        setcookie('auth', 'ok', time()+3600);   
+        setcookie('username', $username, time()+3600);            
+        }
+        }
+       } 
 ?>
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,10 +25,10 @@
     <title>Set Cookie Data</title>
 </head>
 <body>
-    <form action="<?php //<code>// ?>" method="">
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
     Name: <input type="text" name="username">
     Password: <input type="text" name="password"><br/><br/>
-    <input type="submit" value="Log Me In">
+    <input type="submit" name = "submit" value="Log Me In">
     </form>
 </body>
 </html>
--- web4/loggedin.php
+++ web4/loggedin.php
@@ -1,5 +1,10 @@
 <?php
    // test cookies if is set, display if set
+   if(isset($_COOKIE['auth'])){
+    echo 'User '. $_COOKIE['username'] . ' is set<br>';
+} else {
+    header('Location: index.php');
+}
 ?>
 <html lang="en">
 <head>
