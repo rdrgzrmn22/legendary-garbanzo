77e0759854d8b6f1e93968c620e8f8288ba0fb42
unknown <202080482@psu.palawan.edu.ph>
2022-09-19 23:19:43 +0800

    web2 done

--- web2/fav.php
+++ web2/fav.php
@@ -2,7 +2,28 @@
 <head><title>Your Favorite</title></head>
 <body>
     <?php
+       
+        if(isset($_POST['username'])){
+
+            $user = $_POST['username'];
+            $usercheck = empty($user);
+           
+            $b = $_POST['dish']; 
+            $achk = empty($b);
+
         
+                if ($usercheck != 1){       
+                    if(isset($_POST['color']) & $achk != 1){
+                        $a = $_POST['color'];              
+                        echo "Thank you for your selection ".$user. "<br>You really enjoy ". $b . " <br>- especially with a nice " . $a . " wine";
+
+                }else {echo "You haven't either selected a wine or entered a dish."; }
+
+
+                }else {echo "You haven't entered your name."; }
+
+          }
     ?>
+    
 </body>
 </html>
